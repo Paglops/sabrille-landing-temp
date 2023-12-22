c2bfcfa5f4c577f0dbe2cf0abd8bb82a3295274c
Sarah <34034102+Paglops@users.noreply.github.com>
2023-12-22 03:31:02 +0100

    content update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,11 +8,7 @@ export default function Home() {
       <h1>Hello world!</h1>
       <Counter />
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+        More to come soon...
       </p>
     </main>
   );
